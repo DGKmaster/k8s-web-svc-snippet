8e43be04741e86f471c892eb3b12371d30ecc717
DGKmaster <artvolkov96@yandex.ru>
2022-05-15 12:58:49 +0300

    Add simple service.

--- svc/main.go
+++ svc/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "add")
+	})
+
+	http.HandleFunc("/all", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "all")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
 }
